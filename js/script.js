600b25d27f85b0f539c774c5f82a824cbc1ce12d
Damian Fusiek <damas13251@gmail.com>
2020-12-12 19:54:30 +0100

    add javascript

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@
         console.log('links:', links);
     }); */
     const titleClickHandler = function (event) {
+        event.preventDefault();
+        const clickedElement = this;
         console.log('Link was clicked!');
-        console.log(event);
         /*[DONE] remove class 'active' from all article links  */
         const activeLinks = document.querySelectorAll('.titles a.active');
 
         for (let activeLink of activeLinks) {
             activeLink.classList.remove('active');
         }
-        /* [IN PROGRESS] add class 'active' to the clicked link */
-        const clickedElement = this;
+        /*[DONE] add class 'active' to the clicked link */
         console.log('clickedElement:', clickedElement);
-
+        clickedElement.classList.add('active');
         /*[DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -23,10 +23,11 @@
             activeArticle.classList.remove('active');
         }
         /* get 'href' attribute from the clicked link */
-
+        const href = clickedElement.getAttribute('href');
         /* find the correct article using the selector (value of 'href' attribute) */
-
+        const currentArticle = document.querySelector(href);
         /* add class 'active' to the correct article */
+        currentArticle.classList.add('active');
     }
 
     const links = document.querySelectorAll('.titles a');
